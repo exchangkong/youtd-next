11eb2f9cbb79b9a05973bc0997215c8fa86bc88e
exchangkong <438689575@qq.com>
2025-05-13 04:03:34 +0800

    增加header

--- next.config.js
+++ next.config.js
@@ -21,6 +21,10 @@ const nextConfig = {
             key: 'Cross-Origin-Embedder-Policy',
             value: 'require-corp',
           },
+          {
+            key: 'Cross-Origin-Resource-Policy',
+            value: 'cross-origin',
+          },
         ],
       },
     ];
